78998a297175f9a67eb958aa2f691e58431c7e1b
Eric Lau <ericlaucmo@gmail.com>
2021-11-28 13:00:46 -0800

    Implemented "!marbles" command for "admin" users (#2)
    * Added ADMIN_ROLE to env vars
    
    * Fixed date conversion issues and added !marbles command
    
    * Updated raffle command
    
    * Update readme

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,42 @@
 /**
- * Determines the day of Sunday of the week for the requested date
+ * Based on zero-indexed days (Date.getDay())
+ *
+ * Sun - 6 days since Monday
+ * Mon - 0 days since Monday
+ * Tue - 1 day since Monday
+ * Wed - 2 days since Monday
+ * Thu - 3 days since Monday
+ * Fri - 4 days since Monday
+ * Sat - 5 days since Monday
+ */
+const diffFromMonday = [6, 0, 1, 2, 3, 4, 5];
+
+/**
+ * Determines the day of Monday (UTC) of the week for the requested date
  * 	in format of YYYY/MM/DD
+ *
+ * We use UTC Monday, since that's when stuff resets
  *
  * @param {string} date date string in the format of MM/DD/YYYY
  */
-function calcSundayOfWeek(date) {
+function getUTCMondayOfWeekFromDate(date) {
 	// Defaults to today, if not date is requested
 	const requestedDate = date ? new Date(date) : new Date();
 
-	// Value between 0 to 6 (Sun, Mon, Tue,  Wed, Thu, Fri, Sat)
-	const dayOfRequestedDate = requestedDate.getUTCDay();
+	// Value between 1 to 6 (Sun, Mon, Tue,  Wed, Thu, Fri, Sat)
+	/**
+	 * Results in a value between 1 to 6
+	 *
+	 * Sunday is defaulted to 6
+	 * 	Sunday's day value is 0, which is falsy
+	 */
+	const dayOfRequestedDate = diffFromMonday[requestedDate.getUTCDay()];
 
 	const year = requestedDate.getFullYear();
 	// getMonth returns the value of the month in a 0-index fashion
 	const month = requestedDate.getMonth() + 1;
-	// Getting the day for the Sunday for the week of the requested date
-	const day = requestedDate.getUTCDate() - dayOfRequestedDate;
+	// Getting the day for the Monday for the week of the requested date
+	const day = requestedDate.getUTCDate() - dayOfRequestedDate || 1;
 
 	return `${year}/${month}/${day}`;
 }
@@ -31,7 +52,7 @@ function calcSundayOfWeek(date) {
  * @param {string} date date string in the format of MM/DD/YYYY
  */
 function generateSheetRange(range, date) {
-	const sheetName = calcSundayOfWeek(date);
+	const sheetName = getUTCMondayOfWeekFromDate(date);
 
 	if (Array.isArray(range)) {
 		const ranges = range.map((el) => `${sheetName}!${el}`);
@@ -39,6 +60,25 @@ function generateSheetRange(range, date) {
 	}
 
 	return `${sheetName}!${range}`;
+}
+
+// We use UTC Monday, since that's when stuff resets
+function getAllUTCMondaysBetweenWeeks(start, end) {
+	const sheets = [];
+	const startDate = new Date(getUTCMondayOfWeekFromDate(start));
+	const endDate = new Date(getUTCMondayOfWeekFromDate(end));
+
+	let currentDate = startDate;
+
+	while (currentDate <= endDate) {
+		sheets.push(getUTCMondayOfWeekFromDate(currentDate));
+
+		const nextCurrentDate = currentDate;
+		nextCurrentDate.setDate(nextCurrentDate.getUTCDate() + 7);
+		currentDate = nextCurrentDate;
+	}
+
+	return sheets;
 }
 
 /**
@@ -51,7 +91,7 @@ function generateSheetRange(range, date) {
  * 	Returned from spreadsheets.get - https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/get
  */
 function shouldMakeNewSpreadsheet(workbook) {
-	const expectedSheetName = calcSundayOfWeek();
+	const expectedSheetName = getUTCMondayOfWeekFromDate();
 	const workbookDoesNotHaveSheet = workbook.data.sheets.every(
 		(sheet) => sheet.properties.title !== expectedSheetName,
 	);
@@ -60,7 +100,8 @@ function shouldMakeNewSpreadsheet(workbook) {
 }
 
 module.exports = {
-	calcSundayOfWeek,
+	getUTCMondayOfWeekFromDate,
 	generateSheetRange,
+	getAllUTCMondaysBetweenWeeks,
 	shouldMakeNewSpreadsheet,
 };
